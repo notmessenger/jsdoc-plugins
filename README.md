e835bd00393a57e417b25055b24d4231c651dd45
Jeremy Brown <jeremy@notmessenger.com>
2015-05-25 19:57:56 -0500

    Add emberObservesTag plugin

--- README.md
+++ README.md
@@ -28,3 +28,35 @@ include:
     </ul>
 <?js } ?>
 ```
+
+# emberObservesTag
+
+Adds @observes tag to those DocBlocks that should have it
+
+[Ember.js Computed Properties](http://emberjs.com/api/classes/Ember.ComputedProperty.html) observe properties.
+Without needing to populate [@observes tags](https://github.com/notmessenger/jsdoc-plugins#observestag) this
+plugin will automatically add them to the DocBlocks for you during documentation generation.
+
+This plugin supports both the prototype and non-prototype extension syntax:
+
+```
+fullName: function() {
+    ...
+}.property( 'firstName', 'lastName' ),
+
+fullerName: Ember.computed( 'firstName', 'lastName', function() {
+    ...
+})
+```
+
+In order for this tag to display in the generated documentation you will need to modify the `/method.tmpl` file to
+include:
+
+```
+<?js if (data.observes) { ?>
+    <h5>Observes:</h5>
+    <ul>
+        <li><?js= data.observes ?></li>
+    </ul>
+<?js } ?>
+```
---
+++ plugins/emberObservesTag.js
@@ -0,0 +1,49 @@
+/**
+ * Adds @observes tag to those DocBlocks that should have it
+ *
+ * Ember.js Computed Properties {@link http://emberjs.com/api/classes/Ember.ComputedProperty.html} observe properties.
+ * Without needing to populate @observes tags {@link https://github.com/notmessenger/jsdoc-plugins#observestag} this
+ * plugin will automatically add them to the DocBlocks for you during documentation generation.
+ */
+'use strict';
+
+exports.astNodeVisitor = {
+    visitNode: function( node, e, parser, currentSourceName ) {
+        var argumentValues = [];
+
+        if ( 'Property' === node.type && node.value.arguments ) {
+
+            // Gather up each observed property
+            node.value.arguments.forEach( function( argument ) {
+                if ( 'Literal' === argument.type ) {
+                    argumentValues.push( argument.value );
+                }
+            });
+
+            // Only support multi-line comment block
+            if ( new RegExp( "\n" ).test( e.comment ) ) {
+                e.comment = e.comment.replace(
+                                /\n     \*\//g,
+                                "\n     * @observes " + argumentValues.join(', ' ) + "\n     */"
+                            );
+            }
+        }
+    }
+};
+
+/**
+ * Adds support for @observes tag
+ *
+ * This tag will display the properties being observed by an Ember.js Computed Property
+ * {@link http://emberjs.com/api/classes/Ember.ComputedProperty.html} in the generated documentation.
+ */
+exports.defineTags = function( dictionary ) {
+    dictionary.defineTag( 'observes', {
+        mustHaveValue: true,
+        canHaveType: false,
+        canHaveName: false,
+        onTagged: function( doclet, tag ) {
+            doclet.observes = tag.value;
+        }
+    });
+};
